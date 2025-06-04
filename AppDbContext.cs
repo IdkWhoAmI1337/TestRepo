356182d6aab5451068a676a8a1d705f6ae3a7252
Bartosz <bartoszromanowski90@gmail.com>
2025-06-04 12:52:15 +0200

    ds

---
+++ AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using kolokwium2_przyklad.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace kolokwium2_przyklad.Data;
+
+public class AppDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Course> Courses { get; set; }
+    public DbSet<Enrollment> Enrollments { get; set; }
+}
